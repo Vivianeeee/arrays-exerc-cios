c61b09754b0bd8d696ba550a9e4d24896b71f8fa
Vivianeeee <vivianeoliveirandrade@gmail.com>
2021-11-04 21:51:28 -0300

    first commit

---
+++ exercicio.js
@@ -0,0 +1,16 @@
+// unshift()
+console.clear();
+const pessoas4 = ['joaquim','jose', 'silva', 'xavier'];
+let w = pessoas4.unshift('amanda'); // w = 3
+
+console.log(pessoas4);
+console.log(w);
+
+// Remover e inserir elementos
+// pop()
+console.clear();
+const nomes1 = ['amanda', 'joaquim', 'jose', 'silva','xavier'];
+let x = nomes1.pop(); // x = "xavier"
+
+console.log(nomes1);
+console.log(x);
